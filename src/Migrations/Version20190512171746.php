f8d711e9263dd1f96d17ae258e8f9face7a545db
Luis <luis.henriques@uniplaces.com>
2019-05-12 18:37:49 +0100

    Add Day 10 final part

--- src/Migrations/Version20190512171746.php
+++ src/Migrations/Version20190512171746.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190505172417 extends AbstractMigration
+final class Version20190512171746 extends AbstractMigration
 {
     public function getDescription() : string
     {
